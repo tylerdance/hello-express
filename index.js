cc9ae71360c356257edbf436aeb0274f951e281d
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-11-13 12:41:03 -0500

    [index] Add /about and /blog

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ app.get('/', function(req, res) {
     res.send('Hello, SEI')
 })
 
+app.get('/about', function(req, res) {
+    res.send('About page')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('welcome to my blog')
+})
+
 app.listen(8000)
+
+
